5ca8ecf9aa81c3c8c5d519d4916f3bad35213355
RaimondasR <raimondas.r2@gmail.com>
2022-05-11 02:45:33 +0300

    Code updated. EnterTextPage.js, EnterTextComp.js files are added and modified.

---
+++ src/components/EnterTextComp/EnterTextComp.js
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from 'react';
+import http from '../../plugins/http';
+import { useNavigate } from "react-router-dom";
+import './EnterTextCSS.css';
+
+const EnterTextComp = () => {
+  const nav = useNavigate();
+  const [message, setMessage] = useState(null);
+
+  const refs = { 
+    textRef: useRef()   
+  }  
+
+  async function enterText () {
+    const textData = {    
+      text: refs.textRef.current.value // text to enter
+    } 
+    
+    http.post(textData, "enter-text")
+      .then((res) => {
+        setMessage({success: res.success, message: res.message});
+        if (res.success) {
+          console.log("res:",res);
+          console.log("res.id:",res.id);
+          const id = res.id;
+          setMessage(null);
+        //   nav(`/text/${id}/${refs.textRef.current.value}`)
+        }
+      })
+  };
+
+  return (
+    <div className="EnterTextComp d-flex column">
+  
+      <div className="enter-text-div d-flex column a-center">
+
+        <div className="sign-div d-flex a-center fs20"><b>IBM Watson Natural Language Service</b></div>
+        <div className="sign-div d-flex a-center fs20"><b>Text Submit Form</b></div>
+                    
+        <div className="sign-div d-flex a-flex-end mt10 mb0 fs14">
+          <div className="c-gray mr5"><b>Your Text</b></div>
+          <div className="c-red fs11 mb2"><b>REQUIRED</b></div>              
+        </div>
+
+        <textarea className="msg-inp" ref={refs.textRef} placeholder="Type or copy-paste your text here...">            
+        </textarea>
+                  
+        <div className="sign-div d-flex center mt30">
+          <button onClick={enterText}>Enter Text</button>
+        </div>
+
+        {message && <div className="msg-div d-flex center mt15">{message.message}</div>} 
+      </div>
+    </div>
+  );
+};
+
+export default EnterTextComp;
---
+++ src/pages/EnterTextPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EnterTextComp from '../components/EnterTextComp/EnterTextComp';
+
+const CreateTopicPage = () => {
+  return (
+    <div className="EnterTextPage">
+        <EnterTextComp />
+    </div>
+  )
+};
+
+export default CreateTopicPage;
